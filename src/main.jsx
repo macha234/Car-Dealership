52ef06ea06947804cc0a8e52688e4bd12c056fc5
basil <machariabasi@gmail.com>
2025-04-25 11:55:20 +0300

    react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from "react-router"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
